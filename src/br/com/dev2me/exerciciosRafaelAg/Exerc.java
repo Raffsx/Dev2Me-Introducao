6dc30e9f3984575b9a354ecb3c57e43607edfcba
Rafael de Agostinho <52942590+Raffsx@users.noreply.github.com>
2021-03-29 13:06:07 -0300

    Exercício 3

---
+++ src/br/com/dev2me/exerciciosRafaelAg/Exerc.java
@@ -0,0 +1,19 @@
+package br.com.dev2me.exerciciosRafaelAg;
+
+import br.com.dev2me.exerciciosRafaelAg.Exerc3;
+
+public class Exerc {
+	
+	static double n1 = 7;
+	static double n2 = 8;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exerc3 exerc3 = new Exerc3();
+		exerc3.receberNotasSimples();
+
+
+	}
+
+
+}
